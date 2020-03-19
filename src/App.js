c1c359df5a84b4e4fbb4957f7e174d786816d8a8
Shane <ho.wai.loon@accenture.com>
2020-03-19 21:30:54 +0800

    Added link to about page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>My Name is Shane</div>
-        <div>Sometimes Wai Loon</div>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div>
+          My Name is <Link to={`about`}>Chee Khuin</Link>
+        </div>
+        <div>Sometimes CK</div>
         <div>I have a mane</div>
         <div>I eat balloon</div>
-        <p>
-          RSVP S3
-        </p>
-        <div className="acknowledgements">
-          Made by CK, Shane and Lucie
-        </div>
+        <p>RSVP S3</p>
+        <div className='acknowledgements'>Made by CK, Shane and Lucie</div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
